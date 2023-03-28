93ce24eb1b5867094f7be9edf1448d8b1f6b603d
Lazar Stojanović <lakisd98@gmail.com>
2023-03-28 04:59:18 +0200

    Update: available meals to use firebase backend

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,9 +1,33 @@
 import classes from "./AvailableMeals.module.css";
-import meals from "../../assets/dummy-meals.js";
 import Card from "../UI/Card/Card";
 import MealItem from "./MealItem/MealItem";
+import mealsAPI from "../../API/firebase-meals.js";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
+	const [meals, setMeals] = useState([]);
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await mealsAPI.get("/meals.json");
+
+			const loadedMeals = [];
+
+			for (const key in response.data) {
+				loadedMeals.push({
+					id: key,
+					name: response.data[key].name,
+					description: response.data[key].description,
+					price: response.data[key].price,
+				});
+			}
+
+			setMeals(loadedMeals);
+		};
+
+		fetchMeals();
+	}, []);
+
 	const mealsList = meals.map((meal) => (
 		<MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}>
 			{meal.name}
